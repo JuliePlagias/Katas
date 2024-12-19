69e5dedd1a1e9b89860fea724bb6bdabd0181e59
JuliePlagias <julieplagias@gmail.com>
2024-12-19 18:41:32 +0100

    Removed doublons files

--- minimum.js
+++
@@ -1,9 +0,0 @@
-function min(a, b) {
-  if (typeof a == "number" && typeof b == "number") return Math.min(a, b);
-  else if (a < b) {
-    return a;
-  }
-  return b;
-}
-
-console.log(min("Pomme", "Pamplemousse"));
